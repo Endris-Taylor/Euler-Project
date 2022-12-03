7c253625e3c0c674a6038abc62b8972f74a8a2dc
Endris-Taylor <endris.taylor@gmail.com>
2022-12-03 08:19:12 -0600

    Completed Euler 18 solution.

--- solutions/euler_18.py
+++ solutions/euler_18.py
@@ -34,7 +34,37 @@ and requires a clever method! ;o)
 
 
 def main() -> None:
-    print("Solution Coming Soon!")
+
+    triangle_string = """75
+    95 64
+    17 47 82
+    18 35 87 10
+    20 04 82 47 65
+    19 01 23 75 03 34
+    88 02 77 73 07 63 67
+    99 65 04 28 06 16 70 92
+    41 41 26 56 83 40 80 70 33
+    41 48 72 33 47 32 37 16 94 29
+    53 71 44 65 25 43 91 52 97 51 14
+    70 11 33 28 77 73 17 78 39 68 17 57
+    91 71 52 38 17 14 91 43 58 50 27 29 48
+    63 66 04 68 89 53 67 30 73 16 69 87 40 31
+    04 62 98 27 23 09 70 98 73 93 38 53 60 04 23"""
+
+    # Convert string to 'workable' data structure.
+    triangle: list = triangle_string.strip().split('\n')
+
+    for _, index in enumerate(range(0, len(triangle))):
+        triangle[index] = triangle[index].strip().split(' ')
+        triangle[index] = [int(num) for num in triangle[index]]
+
+    for _, line_num in enumerate(range(len(triangle) - 2, -1, -1)):
+        for _, pos in enumerate(range(len(triangle[line_num]))):
+            triangle[line_num][pos] = triangle[line_num][pos] +\
+                                      max(triangle[line_num + 1][pos], triangle[line_num + 1][pos + 1])
+        triangle.pop()
+
+    print(f"The maximum total from top to bottom of the triangle above is {triangle[0][0]}")
 
 
 if __name__ == "__main__":
